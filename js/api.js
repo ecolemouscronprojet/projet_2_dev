e7f6e4a9c4ba74103fb84893e9b31ee9bd8d1130
samuel-lassoie <59116773+samuel-lassoie@users.noreply.github.com>
2024-11-04 19:47:11 +0100

    wip

--- js/api.js
+++ js/api.js
@@ -1,1 +1,35 @@
-fetch('/data/example.json')
+const app = {
+    users: []
+}
+
+app.fetchUsers = async () => {
+    const response = await fetch('/data/users.json');
+    const users = await response.json()
+    app.users = users
+    return users;
+}
+
+app.display = () => {
+    if (app.users.length == 0) {
+        return;
+    }
+    const list = document.querySelector('#user-list')
+    //empty list
+    list.innerHTML = "";
+    if (list) {
+        for (let user of app.users) {
+            list.innerHTML += `<li>${user.index} : ${user.name}</li>`;
+        }
+    }
+}
+
+//BOOTSTRAP APP
+(async function () {
+
+    // récupérer les utilisateurs
+    await app.fetchUsers()
+
+    // AFFICHER LES UTILISATEURS
+    app.display()
+    
+}())
